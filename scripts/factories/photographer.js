f58cefbdfddaa3f4b8ffac33596f9543cacbc467
Zarconos <119136113+Zarconos@users.noreply.github.com>
2023-05-17 17:18:52 +0200

    try

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,8 +7,31 @@ function photographerFactory(data) {
       const article = document.createElement('article');
       const img = document.createElement('img');
       img.setAttribute('src', picture);
+    
+
+      function getPhotographerData(id) {
+
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].id === id) {
+            return data[i];
+          }
+        }
+        return null; 
+      }
+
+
+      const imgLink = document.createElement('a');
+      imgLink.href = `photographer.html?id=${id}`;
+      imgLink.appendChild(img);
+    
       const h2 = document.createElement('h2');
       h2.textContent = name;
+    
+
+      const h2Link = document.createElement('a');
+      h2Link.href = `photographer.html?id=${id}`;
+      h2Link.appendChild(h2);
+    
       const p = document.createElement('p');
       p.textContent = `${city}, ${country}`;
       p.style.color = '#901C1C';
@@ -19,13 +42,14 @@ function photographerFactory(data) {
       priceElement.textContent = `${price}€/jour`;
       priceElement.style.marginTop = '-10px';
     
-      article.appendChild(img);
-      article.appendChild(h2);
+      article.appendChild(imgLink);
+      article.appendChild(h2Link);
       article.appendChild(p);
       article.appendChild(taglineElement);
       article.appendChild(priceElement);
       return article;
     }
+    
   
     return { id, name, tagline, city, country, price, portrait, picture, getUserCardDOM };
   }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,19 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Récupère les données des photographes depuis le fichier JSON
+async function getPhotographers() {
+    const response = await fetch("data/photographers.json");
+    const { photographers } = await response.json();
+    return photographers;
+  }
+  
+  // Affiche les données du photographe correspondant à l'id
+  function displayPhotographer(photographer) {
+    const photographerSection = document.querySelector(".photographer_section");
+    const userCardDOM = photographerFactory(photographer).getUserCardDOM();
+    photographerSection.appendChild(userCardDOM);
+  }
+  
+  // Récupère l'id du photographe depuis l'URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const photographerId = parseInt(urlParams.get('id'));
+
+
